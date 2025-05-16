61ceb6d4f3932efe7462ec0f41b1d2923c5aeaf0
Corolário <21142386+Corolario@users.noreply.github.com>
2025-05-16 02:04:56 -0300

    Update final.py

--- final.py
+++ final.py
@@ -1,9 +1,16 @@
 import time
 import datetime
+import asyncio
+from telegram import Bot
 from playwright.sync_api import sync_playwright
 
-url = "http://192.168.8.58"
+url = "https://www.dell.com/pt-br/outlet/shop"
 intervalo=60
+
+async def envia_msg(mensagem):
+    bot = Bot(token='7977985:AAFkcNNXNaGSNno9DoLCiJQ')
+    chat_id = '7234221214'
+    await bot.send_message(chat_id=chat_id, text=mensagem)
 
 def download_webpage(url):
     with sync_playwright() as p:
@@ -20,9 +27,11 @@ def verificacao():
         conteudo_anterior = arquivo.read()
     if html != conteudo_anterior:
         agora = datetime.datetime.now()
-        print(f"{agora.strftime('%d/%m/%Y %H:%M:%S')} - A pagina foi alterada!")
+        mensagem = f"{agora.strftime('%d/%m/%Y %H:%M:%S')} - A pagina foi alterada!"
+        asyncio.run(envia_msg(mensagem))
         with open("conteudo.txt", "w") as arquivo:
             arquivo.write(html)
+
 while True:
     verificacao()
     time.sleep(intervalo)
